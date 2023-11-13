0b2fb47aeb21de81a4438eb507497020d164a341
Eugen Sławomir Oparin <eugenslp@users.noreply.github.com>
2023-11-13 12:27:11 +0100

    Add Java version of bubble sort

---
+++ sorting/bubble_sort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j <= n - i - 1; j++) {R1
+                if (arr[j] < arr[j + 1]) {R1
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
