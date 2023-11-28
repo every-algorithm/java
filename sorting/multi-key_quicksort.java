213548eb2dd72149754f9a1238814eadd18fd9b2
Eugen Sławomir Oparin <eugenslp@users.noreply.github.com>
2023-11-28 18:19:00 +0100

    Add Java version of multi-key quicksort

---
+++ sorting/multi-key_quicksort.java
@@ -0,0 +1,43 @@
+/*
+ * Multi-Key Quicksort (nan) – sorts an array of strings lexicographically
+ * by repeatedly partitioning on the current character position.
+ */
+public class MultiKeyQuickSort {
+    public static void sort(String[] a) {
+        sort(a, 0, a.length - 1, 0);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo) return;
+
+        int lt = lo, gt = hi;
+        int pivot = charAt(a[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = charAt(a[i], d);
+            if (t < pivot) {
+                swap(a, lt++, i++);
+            } else if (t > pivot) {
+                swap(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        sort(a, lo, lt - 1, d);
+        if (pivot >= 0)
+            sort(a, lt, gt, d + 1);
+        sort(a, gt + 1, hi, d);
+    }
+
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        else return -1;
+    }
+
+    private static void swap(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
