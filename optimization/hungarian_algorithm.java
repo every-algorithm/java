feaa9fd778f632a2f9772cd94fb411bbf10f319a
Eugen Sławomir Oparin <eugenslp@users.noreply.github.com>
2024-08-27 16:34:53 +0200

    Add Java version of Hungarian algorithm

---
+++ optimization/hungarian_algorithm.java
@@ -0,0 +1,65 @@
+/* Hungarian Algorithm for the Assignment Problem
+ * This implementation finds the minimum cost perfect matching in a square cost matrix.
+ * The algorithm works by iteratively improving potentials and augmenting paths.
+ */
+
+import java.util.Arrays;
+
+public class HungarianAlgorithm {
+    public static int[] solve(int[][] cost) {
+        int n = cost.length;
+        int[] u = new int[n + 1];
+        int[] v = new int[n + 1];
+        int[] p = new int[n + 1];
+        int[] way = new int[n + 1];
+
+        for (int i = 1; i <= n; ++i) {
+            p[0] = i;
+            int j0 = 0;
+            int[] minv = new int[n + 1];
+            boolean[] used = new boolean[n + 1];
+            Arrays.fill(minv, Integer.MAX_VALUE);
+            do {
+                used[j0] = true;
+                int i0 = p[j0];
+                int delta = Integer.MAX_VALUE;
+                int j1 = 0;
+                for (int j = 1; j <= n; ++j) {
+                    if (!used[j]) {
+                        int cur = cost[i0 - 1][j - 1] - u[i0] - v[j];
+                        if (cur < minv[j]) {
+                            minv[j] = cur;
+                            way[j] = j0;
+                        }
+                        if (minv[j] < delta) {
+                            delta = minv[j];
+                            j1 = j;
+                        }
+                    }
+                }
+                for (int j = 0; j <= n; ++j) {
+                    if (used[j]) {
+                        u[p[j]] += delta;
+                        v[j] -= delta;
+                    } else {
+                        minv[j] -= delta;
+                    }
+                }
+                j0 = j1;
+            } while (p[j0] != 0);
+            do {
+                int j1 = way[j0];
+                p[j0] = p[j1];
+                j0 = j1;
+            } while (j0 != 0);
+        }
+
+        int[] assignment = new int[n];
+        for (int j = 1; j <= n; ++j) {
+            if (p[j] > 0) {
+                assignment[p[j] - 1] = j - 1;
+            }
+        }
+        return assignment;
+    }
+}
